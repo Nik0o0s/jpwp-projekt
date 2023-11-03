074645c4922d84cd61a05658eb79bf1917870eb5
nikod <nikod@DESKTOP-MQQ4TL2>
2023-11-03 20:30:07 +0100

    git

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            aa = new PictureBox();
+            postac = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)aa).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)postac).BeginInit();
             SuspendLayout();
+            // 
+            // aa
+            // 
+            aa.BackColor = Color.FromArgb(57, 32, 28);
+            aa.Location = new Point(0, 811);
+            aa.Name = "aa";
+            aa.Size = new Size(1264, 174);
+            aa.TabIndex = 2;
+            aa.TabStop = false;
+            aa.Click += aa_Click;
+            // 
+            // postac
+            // 
+            postac.BackgroundImage = Properties.Resources.przod;
+            postac.Location = new Point(0, 0);
+            postac.Name = "postac";
+            postac.Size = new Size(32, 45);
+            postac.TabIndex = 14;
+            postac.TabStop = false;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(780, 450);
+            BackColor = Color.FromArgb(106, 190, 98);
+            ClientSize = new Size(1264, 985);
+            Controls.Add(postac);
+            Controls.Add(aa);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Grzybobranie";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            ((System.ComponentModel.ISupportInitialize)aa).EndInit();
+            ((System.ComponentModel.ISupportInitialize)postac).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+
+        private PictureBox aa;
+
+        private PictureBox postac;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,107 @@ namespace jpwp
 {
     public partial class Form1 : Form
     {
+        int x = 632;
+        bool czyprawo = false, czylewo = false; bool czygora = false, czydol = false;
+        int y = 405;
         public Form1()
         {
             InitializeComponent();
+            postac.Location = new Point(x, y);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if (x != 24 && e.KeyCode == Keys.Left) { x = x - 32; postac.Location = new Point(x, y); postac.BackgroundImage = Properties.Resources.lewo; }
+            ///else if (x != 1208 && e.KeyCode == Keys.Right) { x = x + 32; postac.Location = new Point(x, y); postac.BackgroundImage = Properties.Resources.prawo; }
+            //else if (y != 45 && e.KeyCode == Keys.Up) { y = y - 45; postac.Location = new Point(x, y); postac.BackgroundImage = Properties.Resources.tyl; }
+            //////else if (y != 765 && e.KeyCode == Keys.Down) { y = y + 45; postac.Location = new Point(x, y); postac.BackgroundImage = Properties.Resources.przod; }
+            if (e.KeyCode == Keys.Left)
+            {
+                czylewo = true;
+
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                czyprawo = true;
+
+            }
+
+            else if (e.KeyCode == Keys.Up)
+            {
+                czygora = true;
+
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                czydol = true;
+
+            }
+
+        }
+
+        private void aa_Click(object sender, EventArgs e)
+        {
+            postac.Location = new Point(x - 32, y);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (czylewo)
+            {
+                postac.Location = new Point(postac.Location.X - 5, postac.Location.Y);
+                postac.BackgroundImage = Properties.Resources.lewo;
+
+            }
+            else if (czyprawo)
+            {
+                postac.Location = new Point(postac.Location.X + 5, postac.Location.Y);
+                postac.BackgroundImage = Properties.Resources.prawo;
+
+            }
+
+            else if (czygora)
+            {
+                postac.Location = new Point(postac.Location.X , postac.Location.Y-5);
+                postac.BackgroundImage = Properties.Resources.tyl;
+
+            }
+            else if (czydol)
+            {
+                postac.Location = new Point(postac.Location.X , postac.Location.Y+5);
+                postac.BackgroundImage = Properties.Resources.przod;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                czylewo = false;
+
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                czyprawo = false;
+
+            }
+           else if (e.KeyCode == Keys.Up)
+            {
+                czygora = false;
+
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                czydol = false;
+
+            }
+
         }
     }
 }
